83187dbb5d9ff5ee01b1946df4ac286464f7364f
prabhuaashish <prabhuaashish@gmail.com>
2023-08-24 08:38:43 +0530

    pug

--- first_project/app.js
+++ first_project/app.js
@@ -1,8 +1,23 @@
-const http = require('http');
 const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+
+const adminData = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
 const app = express();
+app.set('view engine', 'pug')
+app.set('views', 'views') 
 
-const server = http.createServer(app);
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public')))
 
-server.listen(3000);
+app.use(adminData.routes)
+
+app.use(shopRoutes)
+
+app.use((req, res, next) => {
+    res.status(404).render('404', {pageTitle: 'Page Not Found'})
+})
+
+app.listen(3000)
